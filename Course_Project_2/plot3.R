bbef2754c60ea6017b9f4f03f6b733044e72249a
ayakimovichz <xiddix@gmail.com>
2015-05-20 22:45:58 +0300

    Update plot3.R

--- Course_Project_2/plot3.R
+++ Course_Project_2/plot3.R
@@ -11,10 +11,10 @@ SCC <- readRDS("./data_temp/exdata_data_NEI_data/Source_Classification_Code.rds"
 library(dplyr)
 data3<- NEI %>%
   filter(fips=="24510") %>%
-  group_by(year, type) %>%
+  group_by(year=as.factor(year), type) %>%
   summarise(Total.Emissions=sum(Emissions))
 library(ggplot2)
 theme_set(theme_grey(base_size = 10)) 
-image3<-qplot(x=year, y=Total.Emissions, data=data3, facets=.~type, geom="line", colour=type,
-      main="Emissions in Baltimore by type, 1999-2008", xlab="Year", ylab="PM2.5 emissions, tons")
+image3<-qplot(x=year, y=Total.Emissions, data=data3, facets=.~type, geom="point", colour=type,
+              main="Emissions in Baltimore by type, 1999-2008", xlab="Year", ylab=expression(paste('PM', ''[2.5], ', tons')))
 ggsave(file="plot3.png", plot=image3, dpi=75, width=200, height=80, units="mm")
