5ddcfbbdc99aaa056bae411518f2cd17474f3868
ayakimovichz <xiddix@gmail.com>
2015-05-20 22:44:44 +0300

    Update plot1.R

--- Course_Project_2/plot1.R
+++ Course_Project_2/plot1.R
@@ -7,12 +7,9 @@ NEI <- readRDS("./data_temp/exdata_data_NEI_data/summarySCC_PM25.rds")
 SCC <- readRDS("./data_temp/exdata_data_NEI_data/Source_Classification_Code.rds")
 
 # plot
-data1<-aggregate(x=NEI[c("Emissions")], FUN=sum, by=list(year=NEI$year))
-plot(x=data1$year, y=data1$Emissions, type="l", col="black", 
-     main="Total emissions from PM2.5 in the US, 1999-2008", xlab="Year", 
-     ylab="PM2.5 emissions, tons")
+data1<-aggregate(x=NEI[c("Emissions")], FUN=sum, by=list(year=as.factor(NEI$year)))
 png(filename="plot1.png", width=480, height=480, units="px")
-plot(x=data1$year, y=data1$Emissions, type="l", col="black", 
-     main="Total emissions from PM2.5 in the US, 1999-2008", xlab="Year", 
-     ylab="PM2.5 emissions, tons")
+barplot(data1$Emissions, names.arg=data1$year, col="orange", 
+     main=expression(paste('Total emissions from PM', ''[2.5], ' in the US')), xlab="Year", 
+     ylab=expression(paste('PM', ''[2.5], ', tons')))
 dev.off()
